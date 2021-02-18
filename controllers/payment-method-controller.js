e39e9dc3c3ce0fc7376ed65783024fc29ffb7644
Ricardo da Gloria <ricardo_h_hop@hotmail.com>
2021-02-18 17:13:49 -0300

    [new] controllers

---
+++ controllers/payment-method-controller.js
@@ -0,0 +1,212 @@
+const mysql = require('../mysql').pool;
+
+function transformPaymentMethod(method) {
+    return method.map(pro => {
+        return {
+            id_payment_method: pro.id_payment_method,
+            description: pro.description,
+            status: pro.status
+        }
+    });
+}
+
+function getPaymentMethod(result) {
+    return {
+        id_payment_method: result[0].id_payment_method,
+        description: result[0].description,
+        status: result[0].status
+    };
+}
+
+
+exports.getPaymentMethod = (req, res, next) => {
+
+    mysql.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        conn.query(
+            'SELECT * FROM payment_methods',
+            (error, result, field) => {
+
+                conn.release();
+                if (error) {
+                    return res.status(500).send(error);
+                }
+
+                const products = transformPaymentMethod(result);
+
+                res.status(200).send(products);
+            }
+        );
+    });
+}
+
+exports.getPaymentMethodForSales = (req, res, next) => {
+
+    mysql.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        conn.query(
+            'SELECT * FROM payment_methods WHERE status = 1 ORDER BY description',
+            (error, result, field) => {
+
+                conn.release();
+                if (error) {
+                    return res.status(500).send(error);
+                }
+
+                const products = transformPaymentMethod(result);
+
+                res.status(200).send(products);
+            }
+        );
+    });
+}
+
+exports.getPaymentMethodById = (req, res, next) => {
+
+    const id = req.params.id;
+
+    mysql.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        conn.query(
+            `SELECT *
+             FROM payment_methods
+             WHERE id_payment_method = ?`,
+            [id],
+            (error, result, field) => {
+                conn.release();
+
+                if (error) {
+                    return res.status(500).send(error);
+                }
+
+                if (!result.length) {
+                    return res.status(404).send('Não encontrado')
+                }
+
+                const product = getPaymentMethod(result);
+                res.status(200).json(product);
+            }
+        );
+    });
+
+}
+
+exports.postPaymentMethod = (req, res, next) => {
+
+    mysql.getConnection((err, connection) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+
+        connection.query(
+            `INSERT INTO payment_methods (description, status)
+             VALUES (?, ?)`,
+            [req.body.description, req.body.status],
+            (error, result, fields) => {
+
+                if (error) {
+                    connection.release();
+                    return res.status(500).send(error);
+                }
+                connection.query(
+                    `SELECT *
+                     FROM payment_methods
+                     WHERE id_payment_method = ?`,
+                    [result.insertId],
+                    (error, result, field) => {
+                        connection.release();
+
+                        if (error) {
+                            return res.status(500).send(error);
+                        }
+
+                        if (!result.length) {
+                            return res.status(404).send('Não encontrado')
+                        }
+
+                        const product = getPaymentMethod(result);
+                        res.status(201).json(product);
+                    }
+                );
+            }
+        );
+    });
+
+}
+
+exports.putPaymentMethod = (req, res, next) => {
+
+    mysql.getConnection((err, connection) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+
+        connection.query(
+            `UPDATE payment_methods
+             SET description = ?,
+                 status      = ?
+             WHERE id_payment_method = ?`,
+            [req.body.description, req.body.status, req.params.id],
+            (error, result, fields) => {
+
+                if (error) {
+                    connection.release();
+                    return res.status(500).send(error);
+                }
+
+                connection.query(
+                    `SELECT *
+                     FROM payment_methods
+                     WHERE id_payment_method = ?`,
+                    [req.params.id],
+                    (error, result, field) => {
+                        connection.release();
+
+                        if (error) {
+                            return res.status(500).send(error);
+                        }
+
+                        if (!result.length) {
+                            return res.status(404).send('Não encontrado')
+                        }
+
+                        const product = getPaymentMethod(result);
+                        res.status(201).json(product);
+                    }
+                );
+            }
+        );
+    });
+}
+
+exports.deletePaymentMethod = (req, res, next) => {
+
+    const id = req.params.id;
+    mysql.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        conn.query(
+            `DELETE
+             FROM payment_methods
+             WHERE id_payment_method = ?`,
+            [id],
+            (error, result, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send(error);
+                }
+
+                res.status(202).send({
+                    message: 'Método de pagamento excluído com sucesso'
+                });
+            }
+        );
+    });
+}
--- middleware/master-auth.js
+++ middleware/master-auth.js
@@ -4,11 +4,12 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decode = jwt.verify(token, process.env.JWT_KEY);
-        req.body.id_user = decode.id_user
-        req.body.permission = decode.permission;
-        next();
+         if(decode.permission === 1) {
+             next();
+         } else {
+             return res.status(401).send({message: 'Falha na autenticação'});
+         }
     } catch (e) {
-        console.log(e)
         return res.status(401).send({message: 'Falha na autenticação'});
     }
 }
