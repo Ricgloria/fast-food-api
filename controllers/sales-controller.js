6fb37bb996779b4d1829232a5aa020ce14c7268c
Ricardo Luiz Ribeiro da Gloria <ricardo.ribeiro@bgmrodotec.com.br>
2021-07-18 21:05:54 -0300

    charts

--- controllers/sales-controller.js
+++ controllers/sales-controller.js
@@ -11,22 +11,22 @@ const dayNameEnum = {
 }
 
 const daysNameArray = [
+    'Sunday',
     'Monday',
     'Tuesday',
     'Wednesday',
     'Thursday',
     'Friday',
-    'Saturday',
-    'Sunday'
+    'Saturday'
 ]
 
 function lastSevenDaysMap() {
     let result = [];
-    let today = new Date();
     for (let i = 0; i < 7; i++) {
-        let newDate = new Date(today.setDate(today.getDate() - 1));
+        let newDate = new Date(new Date().setDate(new Date().getDate() - i));
         result.push({
             name: daysNameArray[newDate.getDay()],
+            day: newDate.getDate(),
             total: 0
         });
     }
@@ -34,14 +34,27 @@ function lastSevenDaysMap() {
 }
 
 function joinDaysOfWeek(lastSevenDays = [], days = []) {
-    days.forEach(day => {
-        const index = lastSevenDays.findIndex(value => value.name === day.name);
-        lastSevenDays[index] = {name: day.name, total: day.total};
+
+    days = days.map(day => {
+        return {
+            name: day.name,
+            day: day.day,
+            total: day.total
+        }
     });
+
+    lastSevenDays.forEach(last => {
+        days.forEach(day => {
+            if (day.day === last.day && day.name === last.name) {
+                last.total = day.total;
+            }
+        });
+    })
+
     return lastSevenDays.map(day => {
         return {
             name: dayNameEnum[day.name],
-            total: day.total
+            total: Number(day.total)
         }
     }).reverse();
 }
@@ -58,7 +71,7 @@ function transformSale(result) {
     })
 }
 
-function reportMap(salesType, paymentMethods, users, deliverymen, itemsSale, total, lastMonth, lastSevenDays) {
+function reportMap(salesType, paymentMethods, users, deliverymen, itemsSale, total, lastMonth, lastSevenDays, sixMonth, allSales) {
     return {
         total: total[0].total,
         salesType,
@@ -67,7 +80,9 @@ function reportMap(salesType, paymentMethods, users, deliverymen, itemsSale, tot
         deliverymen,
         itemsSale,
         lastMonth,
-        lastSevenDays
+        lastSevenDays,
+        sixMonth,
+        allSales
     }
 }
 
@@ -134,63 +149,131 @@ exports.getAllSales = async (req, res) => {
 
 exports.getSalesReports = async (req, res) => {
     try {
-
-        let query = `SELECT sl.name, COUNT(CASE WHEN sl.sales_type_id = sales.sales_type_id THEN 1 END) as total
-                     FROM sales
-                              JOIN sales_type sl on sl.sales_type_id = sales.sales_type_id
-                     GROUP BY sl.name, sales.sales_type_id`;
-        const salesType = await mysql.executeQuery(query, []);
-
-        query = `SELECT pm.description                                                             as name,
-                        COUNT(CASE WHEN pm.id_payment_method = sales.id_payment_method THEN 1 END) as total
-                 FROM sales
-                          JOIN payment_methods as pm on pm.id_payment_method = sales.id_payment_method
-                 GROUP BY pm.description, sales.id_payment_method`;
-        const paymentMethods = await mysql.executeQuery(query, []);
-
-        query = `SELECT u.name, COUNT(sales.id_user) total
-                 FROM sales
-                          JOIN users u on sales.id_user = u.id_user
-                 GROUP BY u.name`;
-        const users = await mysql.executeQuery(query, []);
-
-        query = `SELECT delivery.name, COUNT(sales.id_deliveryman) total
-                 FROM sales
-                          JOIN deliveryman delivery on sales.id_deliveryman = delivery.id_deliveryman
-                 GROUP BY delivery.name`;
-        const deliverymen = await mysql.executeQuery(query, []);
-
-        query = `SELECT p.product_name as name, SUM(sali.amount) as total
-                 FROM sales_items sali
-                          join products p on p.id_product = sali.id_product
-                 GROUP BY p.product_name
-                 ORDER BY total DESC;`
-        const itemsSale = await mysql.executeQuery(query, []);
-
-        query = `SELECT SUM(amount) total
-                 FROM sales_items;`
-        const totalItemsSale = await mysql.executeQuery(query, []);
-
-        query = `SELECT DAY(sa.sale_date) as name, SUM(sa.sale_value) total
-                 FROM sales sa
-                 WHERE MONTH(sa.sale_date) = MONTH(NOW())
-                 GROUP BY name`
-        const lastMonth = await mysql.executeQuery(query, []);
-
+        const salesType = await getSalesTypeReports();
+        const paymentMethods = await getPaymentTypesReports();
+        const users = await getUsersReports();
+        const deliverymen = await getDeliveryManReports();
+        const itemsSale = await getItemSaleReports();
+        const totalItemsSale = await getTotalItemSaleReports();
+        const lastMonth = await getLastMonthReports();
         const lastSevenDays = lastSevenDaysMap();
-
-        query = `SELECT DAYNAME(sa.sale_date) as name, SUM(sa.sale_value) total
-                 FROM sales sa
-                 WHERE sa.sale_date >= NOW() + INTERVAL -7 DAY
-                   AND sa.sale_date < NOW() + INTERVAL 0 DAY
-                 GROUP BY name;`
-
-        const days = await await mysql.executeQuery(query, []);
-
+        const days = await getLastSevenDaysReports();
+        const sixMonth = await getLastSixMonthReports();
+        const allSales = await getAllSalesReports();
         res.status(200).send(reportMap(salesType,
             paymentMethods,
-            users, deliverymen, itemsSale, totalItemsSale, lastMonth, joinDaysOfWeek(lastSevenDays, days)));
+            users, deliverymen, itemsSale, totalItemsSale, lastMonth, joinDaysOfWeek(lastSevenDays, days), sixMonth.reverse(), allSales));
     } catch (e) {
         return res.status(500).send(e);
     }
 }
+
+async function getSalesTypeReports() {
+    let query = `SELECT sl.name, COUNT(CASE WHEN sl.sales_type_id = sales.sales_type_id THEN 1 END) as total
+                 FROM sales
+                          JOIN sales_type sl on sl.sales_type_id = sales.sales_type_id
+                 GROUP BY sl.name, sales.sales_type_id`;
+
+    return await mysql.executeQuery(query, []);
+}
+
+async function getPaymentTypesReports() {
+    const query = `SELECT pm.description                                                             as name,
+                          COUNT(CASE WHEN pm.id_payment_method = sales.id_payment_method THEN 1 END) as total
+                   FROM sales
+                            JOIN payment_methods as pm on pm.id_payment_method = sales.id_payment_method
+                   GROUP BY pm.description, sales.id_payment_method`;
+    return await mysql.executeQuery(query, []);
+}
+
+async function getUsersReports() {
+    const query = `SELECT u.name, COUNT(sales.id_user) total
+                   FROM sales
+                            JOIN users u on sales.id_user = u.id_user
+                   GROUP BY u.name`;
+    return await mysql.executeQuery(query, []);
+}
+
+async function getDeliveryManReports() {
+
+    const query = `SELECT delivery.name, COUNT(sales.id_deliveryman) total
+                   FROM sales
+                            JOIN deliveryman delivery on sales.id_deliveryman = delivery.id_deliveryman
+                   GROUP BY delivery.name`;
+    return await mysql.executeQuery(query, []);
+}
+
+async function getItemSaleReports() {
+    const query = `SELECT p.product_name as name, SUM(sali.amount) as total
+                   FROM sales_items sali
+                            join products p on p.id_product = sali.id_product
+                   GROUP BY p.product_name
+                   ORDER BY total DESC;`
+    return await mysql.executeQuery(query, []);
+}
+
+async function getTotalItemSaleReports() {
+    const query = `SELECT SUM(amount) total
+                   FROM sales_items;`
+    return await mysql.executeQuery(query, []);
+}
+
+async function getLastMonthReports() {
+    const query = `SELECT DATE_FORMAT(sa.sale_date, '%d/%m') as name, SUM(sa.sale_value) total
+                   FROM sales sa
+                   WHERE MONTH(sa.sale_date) = MONTH(NOW())
+                   GROUP BY name;`
+    return await mysql.executeQuery(query, []);
+}
+
+async function getLastSevenDaysReports() {
+    const query = `SELECT DAYNAME(sa.sale_date) as name, DAY(sa.sale_date) as day, SUM(sa.sale_value) total
+                   FROM sales sa
+                   WHERE sa.sale_date >= DATE_SUB(now(), INTERVAL 7 DAY)
+                   GROUP BY name;`
+    return await mysql.executeQuery(query, []);
+}
+
+async function getLastSixMonthReports() {
+    const query = `SELECT DATE_FORMAT(CURDATE(), '%m/%y')               as name,
+                          IF(SUM(sa.sale_value), SUM(sa.sale_value), 0) as total
+                   FROM sales sa
+                   WHERE MONTH(sa.sale_date) = MONTH(NOW())
+                   UNION
+                   SELECT DATE_FORMAT(CURDATE() + INTERVAL - 1 MONTH, '%m/%y') as name,
+                          IF(SUM(sa.sale_value), SUM(sa.sale_value), 0)        as total
+                   FROM sales sa
+                   WHERE MONTH(sa.sale_date) = MONTH(NOW() + INTERVAL - 1 MONTH)
+                   UNION
+                   SELECT DATE_FORMAT(CURDATE() + INTERVAL - 2 MONTH, '%m/%y') as name,
+                          IF(SUM(sa.sale_value), SUM(sa.sale_value), 0)        as total
+                   FROM sales sa
+                   WHERE MONTH(sa.sale_date) = MONTH(NOW() + INTERVAL - 2 MONTH)
+                   UNION
+                   SELECT DATE_FORMAT(CURDATE() + INTERVAL - 3 MONTH, '%m/%y') as name,
+                          IF(SUM(sa.sale_value), SUM(sa.sale_value), 0)        as total
+                   FROM sales sa
+                   WHERE MONTH(sa.sale_date) = MONTH(NOW() + INTERVAL - 3 MONTH)
+                   UNION
+                   SELECT DATE_FORMAT(CURDATE() + INTERVAL - 4 MONTH, '%m/%y') as name,
+                          IF(SUM(sa.sale_value), SUM(sa.sale_value), 0)        as total
+                   FROM sales sa
+                   WHERE MONTH(sa.sale_date) = MONTH(NOW() + INTERVAL - 4 MONTH)
+                   UNION
+                   SELECT DATE_FORMAT(CURDATE() + INTERVAL - 5 MONTH, '%m/%y') as name,
+                          IF(SUM(sa.sale_value), SUM(sa.sale_value), 0)        as total
+                   FROM sales sa
+                   WHERE MONTH(sa.sale_date) = MONTH(NOW() + INTERVAL - 5 MONTH);`
+    return await mysql.executeQuery(query, []);
+}
+
+async function getAllSalesReports() {
+    const query = `SELECT DATE_FORMAT(sa.sale_date, '%m/%y') as name, SUM(sa.sale_value) total
+                   FROM sales sa
+                   GROUP BY name;`
+    return await mysql.executeQuery(query, []);
+}
+
+
+
+
